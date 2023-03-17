ab670469ad420fefea1ae9acd4c5dd00acf409e4
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-03-16 17:21:53 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  * Return: always 0 (success)
  */
-int main (void)
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
