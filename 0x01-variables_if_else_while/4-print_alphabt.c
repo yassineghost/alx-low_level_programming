3258ff5efce9e2506649796e5ca62185754168d7
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-03-20 09:45:22 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	char alphabets[51] = "abcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alphabets[51]);
+	}
+	putchar('\n');
+	return (0);
+}
