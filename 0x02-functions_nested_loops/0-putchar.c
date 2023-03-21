937d9cd1f8cad81897088aa344ea1255181fadd9
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-03-21 16:41:50 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
-/**
- * main - prints _putchar
- *
+#include "main.h"
+/*
+ * main -Entry point
+ * Description:  prints _putchar
  * return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
