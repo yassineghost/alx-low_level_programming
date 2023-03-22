7830c4e29d16811dd1f08c56b10d53ae714ad229
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-03-21 17:40:55 -0700

    alphabets x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,13 @@
 void print_alphabet_x10(void)
 {
 	int i;
-	int j;
+	char c;
 
-	for (j = 1; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = a; j <= z; j++)
+		for (c = a; c <= z; j++)
 		{
-			_putchar (j);
+			_putchar (c);
 		}
 		_putchar ('\n');
 	}
