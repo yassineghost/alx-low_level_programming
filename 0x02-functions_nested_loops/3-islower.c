351b5507afc5acc74d75d49b4eb07d8bb58fc403
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-03-21 18:01:47 -0700

    find lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * int _islower(int c) - prints 1 if the input is lowercase
- * and prints 0 if not
- * Return: 1 for lowercase alphabets, 0 for the rest
+ * int _islower(int c) - prints 1 if the input is lowercase and 0 otherwise
+ *  * @c:  is the int that will use for the argument of the function
+ *  Return : 1 if lowercase else 0
  */
 int _islower(int c)
 {
