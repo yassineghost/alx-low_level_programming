e278b4065c20860853c76b5a47c0b75f93d1b27a
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-03-21 17:55:40 -0700

    find lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.c"
+/**
+ * int _islower(int c) - prints 1 if the input is lowercase
+ * and prints 0 if not
+ * Return: 1 for lowercase alphabets, 0 for the rest
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122);
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar ('\n');
+}
