7140c962e91e2cb98aab5710d970468a466f1aae
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-03-21 17:03:55 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+	int i;
+	for (i = 97; i <=122; i++)
+{
+	_putchar('\n');
+	return (0)
+}
+
