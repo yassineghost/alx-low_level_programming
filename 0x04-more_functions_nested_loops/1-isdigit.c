2e8088da119ab5346604bd3809c3880d68b35d82
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-03-23 15:51:48 -0700

    is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isdigit - checks for a digit (0 through 9)
+* @c: int to be checked
+* Return: Always 1 if c is a digit, 0 otherwiise
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
