1645301f56fb55c5c11afb65a79b44b8e6d56b72
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-03-23 15:59:55 -0700

    multi

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - checks for a digit (0 through 9)
+* @a: first number
+* @b: second number
+* Return: multi result
+*/
+int mul(int a, int b)
+{
+	return ( a * b);
+}
