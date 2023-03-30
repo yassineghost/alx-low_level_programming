d30ebf63527960b1c6d78d23b1c5f4d34721634e
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-03-30 06:12:10 -0700

    numbers base 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,12 @@
  */
 int main(void)
 {
-        int i;
-        for (i = 48; i <= 57; i++)
-        {
-                putchar (i);
-        }
-        putchar ('\n');
-        return (0);
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
