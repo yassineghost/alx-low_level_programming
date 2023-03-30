5332cfdafc886090dad9ea1433dd75c5c40e5dcc
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-03-30 05:47:40 -0700

    base 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-/*
- * main - prints digit numbers of base 10
+/**
+ * main - Prints numbers from 0 to 9.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int i;
-    for (i = 0; i <= 9; i++)
-    {
-        printf ("%d", i);
-    }
-        putchar ('\n');
-        return (0);
+	int i;
 
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
 }
