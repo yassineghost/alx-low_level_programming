4c72ef02cce8c5a8e2688f226842448aa80ede9c
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-04-01 16:40:38 -0700

    swaps

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * swap_int - swaps values
+ *
+ * @a: pointer to swap the value
+ *
+ * @b: pointer to swap the value
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
