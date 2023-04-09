6a67db563da0b7efc3c05a558b0858367406a119
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-04-09 16:26:33 -0700

    argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: arguments vector
+ *
+ * Return: 0
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
