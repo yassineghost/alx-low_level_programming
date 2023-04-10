7a13c00669ae254bc06330494150874e838f6fca
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-04-10 08:03:10 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,13 +13,14 @@ int main(int argc, char **argv)
 {
 	int sum = 0;
 	char *c;
+
 	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
 			if (*c < '0' || *c > '9')
-				return (printf("Error\n"),1);
+				return (printf("Error\n"), 1);
 		sum += atoi(argv[argc]);
 	}
-	printf ("%d\n", sum);
+	printf("%d\n", sum);
 	return (0);
 }
