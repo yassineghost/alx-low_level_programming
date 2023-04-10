4d05b1e85c76d7bb3f5dc37e3100fe6d43e5e6dc
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-04-10 16:54:36 -0700

    libmy

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,11 @@
 #include "main.h"
-/**
- * _puts - print a string followed with a new line
- *
- * @str: char to be printed
- *
- * Retur: 0
- */
-void _puts(char *str)
-{
-	int c = 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-	while (str[c] != '\0')
-	{
-		_putchar (str[c]);
-		c++;
-	}
-	_putchar ('\n');
+void _puts(char *s)
+{
+    puts(s);
 }
+
