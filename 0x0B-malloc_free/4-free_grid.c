3960cb64cb6a19db88557facc3eda18d29efc3ae
yassine moutawakil <yassine.moutawakil00@gmail.com>
2023-04-11 15:56:50 -0700

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid -free the grid
+ *
+ * @grid: grid of memories
+ * @height: int
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int c = 0;
+
+	for (; i <height; c++)
+		free(grid[i]);
+	free(grid);
+}
