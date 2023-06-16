fe04a6faab8766e05ce889f6b7a2d6fb98c4e726
Yassine Moutawakil <yassine.moutawakil00@gmail.com>
2023-06-15 17:25:51 -0700

    nconcat2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,8 +2,7 @@
 #include "main.h"
 
 /**
- * *string_nconcat - concatenates two strings
- *
+ * *string_nconcat - concatenates n bytes of a string to another string
  * @s1: string to append to
  * @s2: string to concatenate from
  * @n: number of bytes from s2 to concatenate to s1
@@ -13,7 +12,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *x;
+	char *s;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
@@ -23,24 +22,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len2++;
 
 	if (n < len2)
-		x = malloc(sizeof(char) * (len1 + n + 1));
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		x = malloc(sizeof(char) * (len1 + len2 + 1));
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (!x)
+	if (!s)
 		return (NULL);
 	while (i < len1)
 	{
-		x[i] = s1[i];
+		s[i] = s1[i];
 		i++;
 	}
 
 	while (n < len2 && i < (len1 + n))
-		x[i++] = s2[j++];
+		s[i++] = s2[j++];
 
 	while (n >= len2 && i < (len1 + len2))
-		x[i++] = s2[j++];
-	x[i] = '\0';
-	return (x);
+		s[i++] = s2[j++];
+	s[i] = '\0';
+	return (s);
 
-
+}
